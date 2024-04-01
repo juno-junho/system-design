9d0b0214e72e704bd2fdd510e4e13f55f62d4427
juno-junho <ssmm0205@naver.com>
2024-04-01 21:56:29 +0900

    feat: shortenUrl로 get 요청 로직 구현

--- src/main/java/com/junho/systemdesign/urlshortener/controller/ShortenUrlController.java
+++ src/main/java/com/junho/systemdesign/urlshortener/controller/ShortenUrlController.java
@@ -6,12 +6,13 @@ import com.junho.systemdesign.urlshortener.service.ShortenUrlService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -24,8 +25,7 @@ public class ShortenUrlController {
     @PostMapping("/api/v1/data/shorten")
     public UrlResponse shortenUrl(@Valid @RequestBody UrlRequest urlRequest) { // todo valid 확인
         log.debug("url inserted : {}", urlRequest.longUrl());
-        // TODO : 1. url DB에 존재? 반환 : 새로운 id 생성 -> DB에 저장 -> 반환  =>DONE
-        String longUrl = urlRequest.longUrl();
+        String longUrl = urlRequest.longUrl();// 1. Long URL DB에 존재 ? 반환 : shorten URL 생성 -> DB에 저장 -> 반환
 
         if (shortenUrlService.isLongUrlExist(longUrl)) {
             String shortenUrl = shortenUrlService.getShortenUrl(longUrl);
@@ -38,15 +38,16 @@ public class ShortenUrlController {
     }
 
     @GetMapping("/{shortenUrl}")
-    @ResponseStatus(HttpStatus.FOUND) // 트래픽 분석을 위해 301대신 302 반환
-    public String getShortenUrl(@PathVariable String shortenUrl) {
-        // TODO: DB 에 있으면 redirect
-        if (shortenUrlService.isShortenUrlExist(shortenUrl)) {
-            // TODO long URL로 redirect -> 방법 찾아보기
-            return "redirect:" + shortenUrlService.getLongUrl(shortenUrl);
+    public ResponseEntity<String> getShortenUrl(@PathVariable String shortenUrl) {
+        if (shortenUrlService.isShortenUrlExist(shortenUrl)) { // DB 에 있으면 redirect
+            String longUrl = shortenUrlService.getLongUrl(shortenUrl);
+            return ResponseEntity.status(HttpStatus.FOUND) // 트래픽 분석을 위해 301대신 302 반환
+                    .header(HttpHeaders.LOCATION, longUrl)
+                    .build();
         }
-        // TODO : 없으면 예외 페이지로 이동
-        return "shorten";
+        return ResponseEntity.status(HttpStatus.NOT_FOUND) // DB에 존재하지 않으면 error page로 redirect
+                .header(HttpHeaders.LOCATION, "/error")
+                .build();
     }
 
 }
